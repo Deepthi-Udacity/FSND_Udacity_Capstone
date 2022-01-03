b353044867b6f428f54071b5dc75b69cc7cebb0c
Deepthi <mithudeepu3112@gmail.com>
2022-01-03 17:04:43 +0530

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-export AUTH0_DOMAIN='udacity3iam.us.auth0.com'
-export API_AUDIENCE='CastingAgency'
+AUTH0_DOMAIN='udacity3iam.us.auth0.com'
+API_AUDIENCE='CastingAgency'
+DATABASE_URL=postgres://kongfkxwwongyq:897d9c27cbd78535c7d8f3b9603cbd018630055662fc2cbaab7ae8981ef0f852@ec2-3-213-41-172.compute-1.amazonaws.com:5432/ddlsms9to8onoo
